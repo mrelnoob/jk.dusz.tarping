0cca178fceb2956a5360727f88199e39e95ae49d
fanf <francois.martin74190@gmail.com>
2020-11-27 12:15:01 +0100

    :bulb: Update documentation

--- R/import_raw_data.R
+++ R/import_raw_data.R
@@ -1,7 +1,7 @@
 
 #' Import the Raw Dataset
 #'
-#' Import the raw .csv file of the 2020 knotweeds' tarping survey data. \cr To avoid errors, please ensure
+#' Import the raw .csv file of the 2020 knotweed's tarping survey data. \cr To avoid errors, please ensure
 #' that there are no ";" in the cells of the CSV (e.g. if comments have been made in the table cells) and
 #' that strings within the table are written in lower cases, without spaces or any special characters
 #' (you can write in English for instance).
